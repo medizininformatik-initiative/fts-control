3dd6e72c855166ccac9e766c4e34a708190d9312
eThiel <ephraim1hiel@gmail.com>
2025-03-03 08:53:39 +0100

    Centralized ToTime()

--- cmd/listProcesses.go
+++ cmd/listProcesses.go
@@ -12,10 +12,6 @@ import (
 
 type TimeArray [7]int
 
-func (t TimeArray) ToTime() time.Time {
-	return time.Date(t[0], time.Month(t[1]), t[2], t[3], t[4], t[5], t[6], time.UTC)
-}
-
 // listProcessesCmd represents the listProcesses command
 var listProcessesCmd = &cobra.Command{
 	Use:   "listProcesses",
